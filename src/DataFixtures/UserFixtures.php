3f01a018fb59a3d6de8eff80576be5975f895478
Pierre Hardy <pierre.hardy1999@gmail.com>
2023-03-02 15:46:40 +0100

    change user fixture, add 4 user role higher than user

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -22,7 +22,7 @@ class UserFixtures extends Fixture
         $faker = Factory::create('fr_FR');
         $slug = new Slugify();
 
-        for($i =0 ; $i <=50; $i++){
+        for($i =0 ; $i <=56; $i++){
            $user = new User();
            $user->setFirstname($faker->firstName())
                 ->setLastname($faker->lastName())
@@ -33,9 +33,61 @@ class UserFixtures extends Fixture
                 ->setUpdatedAt(new \DateTimeImmutable())
                 ->setBirthday($faker->dateTime())
                 ->setIsDesactivated($faker->boolean(10))
-                ->setRoles([$faker->randomElement(['ROLE_USER','ROLE_SUPPORT','ROLE_MODERATOR','ROLE_ADMIN','ROLE_SUPER_ADMIN'])]);
+                ->setRoles(['ROLE_USER']);
            $manager->persist($user);
         }
+
+        $user = new User();
+        $user->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setUsername($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())))
+            ->setEmail($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())). '.'. $faker->numberBetween(1000,9999) .'@'. $faker->freeEmailDomain())
+            ->setPassword($this->hasher->hashPassword($user, 'password'))
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable())
+            ->setBirthday($faker->dateTime())
+            ->setIsDesactivated($faker->boolean(10))
+            ->setRoles(['ROLE_SUPPORT']);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setUsername($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())))
+            ->setEmail($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())). '.'. $faker->numberBetween(1000,9999) .'@'. $faker->freeEmailDomain())
+            ->setPassword($this->hasher->hashPassword($user, 'password'))
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable())
+            ->setBirthday($faker->dateTime())
+            ->setIsDesactivated($faker->boolean(10))
+            ->setRoles(['ROLE_MODERATOR']);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setUsername($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())))
+            ->setEmail($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())). '.'. $faker->numberBetween(1000,9999) .'@'. $faker->freeEmailDomain())
+            ->setPassword($this->hasher->hashPassword($user, 'password'))
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable())
+            ->setBirthday($faker->dateTime())
+            ->setIsDesactivated($faker->boolean(10))
+            ->setRoles(['ROLE_ADMIN']);
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setFirstname($faker->firstName())
+            ->setLastname($faker->lastName())
+            ->setUsername($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())))
+            ->setEmail($slug->slugify($user->getFirstname(). '.' . $slug->slugify($user->getLastname())). '.'. $faker->numberBetween(1000,9999) .'@'. $faker->freeEmailDomain())
+            ->setPassword($this->hasher->hashPassword($user, 'password'))
+            ->setCreatedAt(new \DateTimeImmutable())
+            ->setUpdatedAt(new \DateTimeImmutable())
+            ->setBirthday($faker->dateTime())
+            ->setIsDesactivated($faker->boolean(10))
+            ->setRoles(['ROLE_SUPER_ADMIN']);
+        $manager->persist($user);
 
         $manager->flush();
     }
